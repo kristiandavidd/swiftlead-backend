316268944f9f288482a80bfde370da8e5e924e53
kristiandavidd <kristiandavid644@gmail.com>
2024-10-22 23:04:10 +0700

    fix: firebase config

--- config/firebase.js
+++ config/firebase.js
@@ -1,11 +1,26 @@
-require('dotenv').config();
-const path = require('path');
+// require('dotenv').config();
+// const path = require('path');
 
+// const admin = require('firebase-admin');
+// const serviceAccountPath = process.env.FIREBASE_CREDENTIALS;
+
+// admin.initializeApp({
+//     credential: admin.credential.cert(require(path.resolve(serviceAccountPath))),
+//     databaseURL: process.env.FIREBASE_DB_URL,
+// });
+
+// const db = admin.database();
+
+// module.exports = { db };
+
+// railway config
+require('dotenv').config();
 const admin = require('firebase-admin');
-const serviceAccountPath = process.env.FIREBASE_CREDENTIALS;
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
 
 admin.initializeApp({
-    credential: admin.credential.cert(require(path.resolve(serviceAccountPath))),
+    credential: admin.credential.cert(serviceAccount),
     databaseURL: process.env.FIREBASE_DB_URL,
 });
 
