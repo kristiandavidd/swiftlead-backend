5beb1468289978aaa9859a3f7c56433ffac00d67
kristiandavidd <kristiandavid644@gmail.com>
2024-10-23 00:10:39 +0700

    update: cors policy

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ const server = http.createServer(app);
 const io = socketIo(server, {
     path: '/socket.io',
     cors: {
-        origin: 'http://localhost:3000',
+        origin: '*',
         methods: ['GET', 'POST'],
     },
 });
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
 }));
