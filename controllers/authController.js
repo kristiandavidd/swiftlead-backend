b5f8f1a405ae1b94662090599f013deee2564c22
kristiandavidd <kristiandavid644@gmail.com>
2024-10-29 11:16:51 +0700

    fix: auth controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,56 +3,37 @@ const db = require('../config/db');
 
 const saltRounds = 10;
 
-const register = (req, res) => {
-    const { username, email, password } = req.body;
+const register = async (req, res) => {
+    const { email, password } = req.body;
     console.log('Registering user:', req.body);
-    console.log('Headers:', req.headers);
-    console.log('Headers:', {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-    });
 
-    db.query('SELECT * FROM users WHERE email = ?', [email], async (err, result) => {
-        if (err) {
-            return res.status(500).json({ message: 'Database query error' });
-        }
-
+    try {
+        const [result] = await db.query('SELECT * FROM users WHERE email = ?', [email]);
         if (result.length > 0) {
             return res.status(400).json({ message: 'Email already exists' });
         }
         console.log('registration start...');
 
-        try {
-            const hashedPassword = await bcrypt.hash(password, saltRounds);
-            console.log('hashed password')
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        console.log('hashed password');
 
-            db.query('INSERT INTO users (email, password) VALUES (?, ?)',
-                [email, hashedPassword],
-                (err, result) => {
-                    if (err) {
-                        console.error('Error inserting user into database:', err);
-                        return res.status(500).json({ message: 'Server error' });
-                    }
-                    console.log('user inserted');
+        const [insertResult] = await db.query('INSERT INTO users (email, password) VALUES (?, ?)', [email, hashedPassword]);
+        console.log('user inserted');
 
-                    res.status(201).json({ message: 'User registered successfully!' });
-                }
-            );
-        } catch (error) {
-            console.error('Error hashing password:', error);
-            res.status(500).json({ message: 'Server error' });
-        }
-        console.log('registration end...');
-    });
+        res.status(201).json({ message: 'User registered successfully!' });
+    } catch (error) {
+        console.error('Database query error:', error);
+        res.status(500).json({ message: 'Server error' });
+    }
+    console.log('registration end...');
 };
 
-const login = (req, res) => {
+
+const login = async (req, res) => {
     const { email, password } = req.body;
 
-    db.query('SELECT * FROM users WHERE email = ?', [email], async (err, result) => {
-        if (err) {
-            return res.status(500).json({ message: 'Database query error' });
-        }
+    try {
+        const [result] = await db.query('SELECT * FROM users WHERE email = ?', [email]);
 
         if (result.length === 0) {
             return res.status(404).json({ message: 'User not found' });
@@ -66,7 +47,10 @@ const login = (req, res) => {
         }
 
         res.status(200).json({ message: 'Login successful', user: { email: user.email } });
-    });
+    } catch (err) {
+        console.error('Database query error:', err);
+        res.status(500).json({ message: 'Server error' });
+    }
 };
 
 module.exports = { register, login };
